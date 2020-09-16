2e056c5391a0559216df3368dc98b9c79bf3c6e5
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2020-09-16 01:07:28 -0300

    Ajustar services

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -14,23 +14,23 @@ export class PostagemService {
   }
 
   getAllPostagens(){
-    return this.http.get('http://localhost:8080/postagem', this.token);
+    return this.http.get('https://clippy-back-end.herokuapp.com/postagem', this.token);
   }
 
   getByIdPostagem(id:number){
-    return this.http.get(`http://localhost:8080/postagem/${id}`, this.token)
+    return this.http.get(`https://clippy-back-end.herokuapp.com/postagem/${id}`, this.token)
    }
 
   postPostagem(postagem: PostagemModel){
-    return this.http.post('http://localhost:8080/postagem', postagem, this.token);
+    return this.http.post('https://clippy-back-end.herokuapp.com/postagem', postagem, this.token);
   }
 
   putPostagem(postagem: PostagemModel){
-    return this.http.put("http://localhost:8080/postagem", postagem, this.token)
+    return this.http.put("https://clippy-back-end.herokuapp.com/postagem", postagem, this.token)
   }
 
   deletePostagem(id:number){
-    return this.http.delete(`http://localhost:8080/postagem/${id}`, this.token)
+    return this.http.delete(`https://clippy-back-end.herokuapp.com/postagem/${id}`, this.token)
   }
 
   getByTituloPostagem(titulo: string) {
--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -14,23 +14,23 @@ export class TemaService {
   }
 
   getAllTemas(){
-    return this.http.get('http://localhost:8080/tema', this.token);
+    return this.http.get('https://clippy-back-end.herokuapp.com/tema', this.token);
   }
 
   getByIdTema(id: number){
-    return this.http.get(`http://localhost:8080/tema/${id}`, this.token);
+    return this.http.get(`https://clippy-back-end.herokuapp.com/tema/${id}`, this.token);
   }
 
   postTema(tema: TemaModel) {
-    return this.http.post('http://localhost:8080/tema', tema, this.token)
+    return this.http.post('https://clippy-back-end.herokuapp.com/tema', tema, this.token)
   }
 
   putTema(tema: TemaModel){
-    return this.http.put('http://localhost:8080/tema',tema, this.token)
+    return this.http.put('https://clippy-back-end.herokuapp.com/tema',tema, this.token)
   }
   
   deleteTema(id: number){
-    return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)
+    return this.http.delete(`https://clippy-back-end.herokuapp.com/tema/${id}`, this.token)
   }
 
 }
